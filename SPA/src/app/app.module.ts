7af33a3692947172f39009c22a322d60f6d47a8a
Caio Peres <caio@nork.digital>
2021-12-28 20:37:26 -0300

    fix

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { rootRouterConfig } from './app.routes';
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
@@ -12,10 +15,10 @@ import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
+import { rootRouterConfig } from './app.routes';
 import { DataBindingComponent } from './demos/data-binding/data-binding.component';
-import { ProdutoService } from './produtos/produtos.serivce';
-import { ListaProdutosComponent } from './produtos/lista-produtos/lista-produtos.component';
-
+import { ProdutoService } from './produtos/produtos.service';
+import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
 
 @NgModule({
   declarations: [
@@ -26,17 +29,17 @@ import { ListaProdutosComponent } from './produtos/lista-produtos/lista-produtos
     SobreComponent,
     ContatoComponent,
     DataBindingComponent,
-    ListaProdutosComponent
+    ListaProdutoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClient,
-    [RouterModule.forRoot(rootRouterConfig, { useHash: false })]
+    HttpClientModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
   ],
   providers: [
     ProdutoService,
-    { provide: APP_BASE_HREF, useValue: '/' }
+    {provide: APP_BASE_HREF, useValue: '/'}
   ],
   bootstrap: [AppComponent]
 })
